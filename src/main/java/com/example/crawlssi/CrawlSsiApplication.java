8cf8342929235563f36877e3a70c8978b5f84a63
Long Nguyen <anonyhost@gmail.com>
2021-04-27 08:33:55 +0700

    first commit

---
+++ src/main/java/com/example/crawlssi/CrawlSsiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crawlssi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlSsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlSsiApplication.class, args);
+    }
+
+}
